2086311030e070d7b6220e909299b3bdb9595390
ojthesixth <110111400+ojthesixth@users.noreply.github.com>
2022-08-29 12:30:20 +0200

    plateformer03
    script, struggling with jump

--- Assets/control_dwarf.cs
+++ Assets/control_dwarf.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class control_dwarf : MonoBehaviour
-{   public float _speed = 10f;
+{   
+    private float controlDwarf;
+    public float _speed = 10f;
     public GameObject Dwarf;
     public Rigidbody2D _rigidbody;
+    public CapsuleCollider2D _collider2D;
+    public Animator _animator;
+    public float GravityScaleOnFall;
+    public Vector2 direction;
+    
+    public float walkingSpeed;
+    [Header("jump")]
+    public bool isJumping = false;
+    public byte currentJumpCount = 0;
+    //public int jumpPower = 1000;
+    private float _jumpForce = 200f;
+    private float _fallMutiplier = 3f;
 
+    [Range(1, 10)]
+    public float jumpVelocity;
 
     void Start()
     {
@@ -16,9 +32,56 @@ public class control_dwarf : MonoBehaviour
     void Update()
     {
 
-     float v = Input.GetAxis("Horizontal") * _speed;
-        _rigidbody.velocity = new Vector3();
+        controlDwarf = Input.GetAxisRaw("Horizontal");
+        _rigidbody.velocity = new Vector2(controlDwarf * _speed, _rigidbody.velocity.y);
 
+        //part of attempt1
+        /* if (Input.GetButtonDown("Jump"))
+            {
+                isJumping = true;
+            }
+
+        }*/
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+
+    void fixedUpdate()
+        {
+
+          
+        //attempt1 at jump
+        /* direction.y = _rigidbody.velocity.y;
+
+        if (isJumping)
+        {
+            direction.y = _jumpForce * Time.fixedDeltaTime;
+            isJumping = false;
+        }
+        if(_rigidbody.velocity.y < 0)
+        {
+            _rigidbody.gravityScale = _fallMutiplier;
+        }
+        else
+        {
+            _rigidbody.gravityScale = 1f;
+        }
+
+        _rigidbody.velocity = direction;
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       /* if (collision.collider.CompareTag("Floor"))
+        {
+            _nbJump = 0;
+        }
+        */
+    }
+
 }
