1235db4a10642ca4768963ffe75da3e7f297adb5
ojthesixth <110111400+ojthesixth@users.noreply.github.com>
2022-10-24 10:19:26 +0200

    platerformer08
    updated

--- Assets/pitfall.cs
+++ Assets/pitfall.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 public class pitfall : MonoBehaviour
 {
-    public GameObject Dwarf _dwarf;
+    /* public GameObject Dwarf _dwarf;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     // Start is called before the first frame update
+     void Start()
+     {
 
-    // Update is called once per frame
-    void Update()
-    {
-        void Update()
-        {
-            if (_dwarf //touches sprite then dead )
-            {
-                Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
-            }
+     }
 
-        }
-    }
+     // Update is called once per frame
+     void Update()
+     {
+         void Update()
+         {
+             if (_dwarf //touches sprite then dead )
+             {
+                 Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+             }
+
+         }
+     */
+}
